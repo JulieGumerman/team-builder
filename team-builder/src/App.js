8eb37d01a821e1cf1b2cc43aac58e7b308805a02
bodhibrdr <julie.gumerman@gmail.com>
2019-08-21 14:12:32 -0600

    team members added in the console.log, form built

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [teamMember, addTeamMember] = useState( [{name: "", birthday: "", favFood: "", allergies: "", more: "", none: ""}] );
+
+  const handleInfo = teacher => {
+    addTeamMember({...teamMember, [teacher.target.name]: teacher.target.value })
+  }
+
+  const submitForm = event => {
+    event.preventDefault();
+    console.log(teamMember);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Teachers! Let's hear about you!</h1>
+      <div className="form-div">
+        <form onSubmit={submitForm}>
+          <label>Member Name:</label>
+          <input 
+            type="text"
+            name="name"
+            placeholder="name goes here"
+            value={teamMember.name}
+            onChange={handleInfo}
+          />
+          <label>Birthday: </label>
+          <input 
+            type="date"
+            name="birthday"
+            value={teamMember.birthday}
+            onChange={handleInfo}
+          />
+          <label>Favorite Food: </label>
+          <input 
+            type="text"
+            placeholder="Cheesecake?"
+            name="favFood"
+            value={teamMember.favFood}
+            onChange={handleInfo}
+          />
+          <label>Food allergies?</label>
+          <input 
+            type="text"
+            placeholder="n/a if none"
+            name="allergies"
+            value={teamMember.allergies}
+            onChange={handleInfo}
+          />
+          <label>What you could use more of:</label>
+          <input 
+            type="text"
+            placeholder="flare pens? post its?"
+            name="more"
+            value={teamMember.more}
+            onChange={handleInfo}
+          />
+          <label>What you don't want more of:</label>
+          <input 
+            type="text"
+            placeholder="grading papers?"
+            name="less"
+            value={teamMember.less}
+            onChange={handleInfo}
+            />
+          <button type="submit">Submit!</button>
+        </form>
+        {console.log(teamMember)}
+      </div>
     </div>
   );
 }
